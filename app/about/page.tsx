e7528189e15292438c2b0697068cd9c2002a0c01
anshuman557 <anshumansingh12457@gmail.com>
2025-06-21 13:58:08 +0530

    feat:changing title for about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,14 @@
+import type { Metadata } from "next";
 import { Suspense } from "react";
 import { AboutHero } from "./about-hero";
 import { TeamSection } from "./team-section";
 import { VisionSection } from "./vision-section";
 
+export const metadata: Metadata = {
+  title: "About| Catfin",
+  description:
+    "Catfin as a brand owns multiple companies for the Management Systems Covering Quality Management System and Environmental Management System for which patents have been granted and applied for.",
+};
 const About = () => {
   return (
     <div>
